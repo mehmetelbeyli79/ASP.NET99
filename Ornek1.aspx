1ee3ce4c8cf74030c33970ebe8eac76da0bc697f
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-02-19 00:05:26 +0300

    Add project files.

---
+++ Ornek1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ornek1.aspx.cs" Inherits="Bakara2_32.Ornek1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <?-- Bu Örnekte PostBack Nasıl yapılır ve Sunucu Cevabı Konuları İşlendi -->
+            <asp:Label ID="ad" runat="server" Text="Label"></asp:Label><br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ornek1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bakara2_32
+{
+    public partial class Ornek1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)//Sayfa Postalanmamış gönderilmemiş ise
+            {
+                Response.Write("Sayfa Henüz Gönderilmedi");
+            }
+            else
+            {
+                Response.Write("Sayfa Gönderildi");
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ad.Text = "Mehmet Elbeyli";
+        }
+    }
+}
