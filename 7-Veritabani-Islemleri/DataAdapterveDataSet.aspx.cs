f9118e1d44c9f8bbafe102d3197465b7d7b01e9e
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-02-23 22:45:35 +0300

    Veritabanı nesneleri tamam

---
+++ 7-Veritabani-Islemleri/DataAdapterveDataSet.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Bakara2_32._7_Veritabani_Islemleri
+{
+    public partial class DataAdapterveDataSet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=" + Server.MapPath("./ornek_veritabani.accdb"));
+            baglan.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * from müşteriler", baglan);//Veritabanı için bir adaptör oluyor
+            DataSet ds = new DataSet();//Veritabanı için bir veri seti hazırlanıp
+            da.Fill(ds);//Adaptördeki tüm veriler Bu veri setinin içine atılıyor
+
+            GridView1.DataSource = ds;//Gridview'e veri yolu olarak gösterilip
+            GridView1.DataBind();//Data içeriye alınıyor.
+        }
+    }
+}
---
+++ 7-Veritabani-Islemleri/DataTableveOledbCommand.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Bakara2_32._7_Veritabani_Islemleri
+{
+    public partial class DataTableveOledbCommand : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Veri Görüntülemede DataAdapter ve DataTable Olayı
+            /*
+                DataTable'da OledbCommand ile Oluşturulan komut yapısı Datareader ve ExecuteReader ile okunup DataTable'a aktarılıyor ve Gridview'in datasource'una yazılır
+                
+                DataTable -> OledbCommand -> DataReader -> ExecuteReader -> Load metodu
+
+             */
+
+            /*
+             
+                DataAdapter'da ise Komut yapısı direkt kendi içinde oluşturulup dataset içini doldurulur Fill metodu ile ve artık Gridview için data source verilir.
+
+                DataAdapter -> DataSet -> Fill metodu
+             
+                Bu yöntem daha kolay görünüyor.
+             */
+
+
+            OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=" + Server.MapPath("./ornek_veritabani.accdb"));
+            baglan.Open();
+
+            DataTable veriler=new DataTable();
+            OleDbCommand cmd = new OleDbCommand("Select * From müşteriler", baglan);
+            OleDbDataReader dr = cmd.ExecuteReader();
+            veriler.Load(dr);
+            GridView1.DataSource = veriler;
+            GridView1.DataBind();
+            baglan.Close();
+        }
+    }
+}
