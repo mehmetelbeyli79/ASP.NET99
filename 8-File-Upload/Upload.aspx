c73a227e6b6aecff6a89e0f0428a2287417859e7
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-03-13 14:41:49 +0300

    File-Upload Konrolü ile Dosya Kaydetme İşlemleri Yapıldı

---
+++ 8-File-Upload/Upload.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="Bakara2_32._8_File_Upload.Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button1" runat="server" Text="GÖNDER" OnClick="Button1_Click" Height="22px" Width="107px" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Font-Size="Large" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 8-File-Upload/Upload.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bakara2_32._8_File_Upload
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+                try
+                {
+                    if (FileUpload1.PostedFile.ContentType == "image/jpeg")
+                    {
+                        if (FileUpload1.PostedFile.ContentLength < 102400)
+                        {
+                            FileUpload1.SaveAs(Server.MapPath("./Uploaded_Files/") + FileUpload1.FileName);
+                            Label1.Text = "Dosya Adı: " +
+                                FileUpload1.PostedFile.FileName +
+                                "<br />Dosya Boyutu: " +
+                                FileUpload1.PostedFile.ContentLength +
+                                "<br />Dosya Türü: " +
+                                FileUpload1.PostedFile.ContentType;
+                        }
+                        else
+                        {
+                            Label1.Text = "Maksimum boyut 100 KB olmalı.";
+                        }
+                    }
+                    else
+                    {
+                        Label1.Text = "Resim dosyası seçin.";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = "Hata Oluştu: " + ex.Message.ToString();
+                }
+            else
+            {
+                Label1.Text = "Dosya Seçin ve GÖNDER Butonuna Tıklayın.";
+            }
+        }
+    }
+}
